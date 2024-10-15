1be6c268b5722071461842db6d6247d5769aed17
charles <charles.angila@student.moringaschool.com>
2024-10-15 11:22:18 +0300

    commit 2

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
 
@@ -7,6 +8,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     address = db.Column(db.String(120), unique=True, nullable=False)
+    posts = db.relationship('Post', back_populates='user')
+    
+    @validates('address')
+    def  validate_address(self, key, address):
+        if '@' not in address:
+            raise ValueError("Invalid email address")
+        return address
     
     
   # Use 'email' instead of 'emails'
@@ -16,4 +24,13 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     description = db.Column(db.String, nullable=False)
-    # user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)  
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    user = db.relationship('User', back_populates='posts')  
+    
+    
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))    
